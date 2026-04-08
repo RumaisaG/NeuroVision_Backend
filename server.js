bbe62de7fc69542e352ac00b548e0ae2c1a63384
RumaisaG <rumaisa.g28@gmail.com>
2026-04-08 11:12:10 +0400

    Improve CORS configuration

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))) 
 
 
 // Routes
